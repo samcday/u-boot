a7e955c5947ce58a7004eb48f47fd9a9826cb772
Sam Day <me@samcday.com>
2025-01-24 21:48:44 +0100

    mach-snapdragon: support including qcom-priv header in multiple places
    The current approach of defining inline no-ops in the header results in
    duplicate symbols when this file is included in additional source units.
    
    Signed-off-by: Sam Day <me@samcday.com>

--- arch/arm/mach-snapdragon/board.c
+++ arch/arm/mach-snapdragon/board.c
@@ -306,7 +306,9 @@ void __weak qcom_board_init(void)
 int board_init(void)
 {
 	show_psci_version();
+	#if CONFIG_IS_ENABLED(OF_LIVE)
 	qcom_of_fixup_nodes();
+	#endif
 	qcom_board_init();
 	return 0;
 }
@@ -517,7 +519,9 @@ int board_late_init(void)
 	qcom_late_init();
 
 	/* Configure the dfu_string for capsule updates */
+	#if IS_ENABLED(CONFIG_EFI_HAVE_CAPSULE_SUPPORT)
 	qcom_configure_capsule_updates();
+	#endif
 
 	return 0;
 }
