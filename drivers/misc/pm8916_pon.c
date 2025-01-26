e7d67f5b11b35b739501fba089e73184eb0d1d45
Sam Day <me@samcday.com>
2025-01-26 11:46:48 +0100

    fixup! drivers: misc: pm8916_pon: add glue driver

--- drivers/misc/pm8916_pon.c
+++ drivers/misc/pm8916_pon.c
@@ -8,16 +8,55 @@
  * (C) Copyright 2025 Linaro Ltd.
  */
 
+#define LOG_DEBUG
+
 #include <dm.h>
 #include <dm/lists.h>
 #include <dm/device_compat.h>
+#include <power/pmic.h>
 #include "button/qcom-pmic.h"
+
+#define PON_REV2			0x01
+
+struct pm8916_pon_priv {
+	struct udevice *pmic;
+	phys_addr_t base;
+	u32 revision;
+};
+
+static int pm8916_pon_probe(struct udevice *dev)
+{
+	int ret;
+	struct pm8916_pon_priv *priv = dev_get_priv(dev);
+
+	priv->pmic = dev->parent;
+	if (!priv->pmic || !device_is_compatible(priv->pmic, "qcom,pm8916")) {
+		dev_err(dev, "parent is not qcom,pm8916 compatible\n");
+		return -EINVAL;
+	}
+
+	priv->base = dev_read_addr(dev);
+
+	if (!priv->base) {
+		dev_err(dev, "missing reg base\n");
+		return -EINVAL;
+	}
+
+	ret = pmic_reg_read(priv->pmic, priv->base + PON_REV2);
+	if (ret < 0) {
+		dev_err(dev, "failed to determine PON rev: %d\n", ret);
+		return ret;
+	}
+
+	priv->revision = ret;
+	dev_dbg(dev, "PON rev: %d\n", priv->revision);
+
+	return 0;
+}
 
 static int pm8916_pon_bind(struct udevice *dev)
 {
-	int ret;
-
-	ret = button_qcom_pmic_setup(dev);
+	int ret = button_qcom_pmic_setup(dev);
 	if (ret)
 		dev_warn(dev, "failed to bind qcom_pwrkey: %d\n", ret);
 
@@ -30,8 +69,10 @@ static const struct udevice_id pm8916_pon_ids[] = {
 };
 
 U_BOOT_DRIVER(pm8916_pon) = {
-	.name = "pm8916_pon",
-	.id = UCLASS_MISC,
-	.of_match = pm8916_pon_ids,
-	.bind = pm8916_pon_bind,
+	.name 		= "pm8916_pon",
+	.id 		= UCLASS_MISC,
+	.of_match 	= pm8916_pon_ids,
+	.bind 		= pm8916_pon_bind,
+	.probe 		= pm8916_pon_probe,
+	.priv_auto	= sizeof(struct pm8916_pon_priv),
 };
