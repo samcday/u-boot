89dfe2512b5b1c747eada2b2c3990e1d98ac697e
Sam Day <me@samcday.com>
2025-01-26 10:46:22 +0100

    drivers: misc: pm8916_pon: add glue driver
    Initially, this driver binds the existing qcom_pwrkey driver to continue
    supporting pwrkey/resin buttons. In follow-up commits it will be further
    extended to bind a reboot-mode driver.
    
    The "qcom,pm8916-pon" compatible is removed from qcom_pwrkey, since
    pm8916_pon owns it and handles binding qcom_pwrkey.
    
    Signed-off-by: Sam Day <me@samcday.com>

---
+++ drivers/misc/pm8916_pon.c
@@ -0,0 +1,78 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Qualcomm PM8916 PON glue driver
+ *
+ * The PON device handles pwrkey/resin buttons, as well as setting reboot
+ * type and reboot mode. This glue driver binds the respective drivers.
+ */
+
+#include <dm.h>
+#include <dm/lists.h>
+#include <dm/device_compat.h>
+#include <power/pmic.h>
+#include "button/qcom-pmic.h"
+
+#define PON_REV2 0x01
+
+struct pm8916_pon_priv {
+	struct udevice *pmic;
+	phys_addr_t base;
+	u32 revision;
+};
+
+static int pm8916_pon_probe(struct udevice *dev)
+{
+	int ret;
+	struct pm8916_pon_priv *priv = dev_get_priv(dev);
+
+	priv->pmic = dev->parent;
+	if (!priv->pmic) {
+		dev_err(dev, "PMIC driver not found\n");
+		return -EINVAL;
+	}
+
+	priv->base = dev_read_addr(dev);
+
+	if (!priv->base) {
+		dev_err(dev, "missing reg base\n");
+		return -EINVAL;
+	}
+
+	ret = pmic_reg_read(priv->pmic, priv->base + PON_REV2);
+	if (ret < 0) {
+		dev_err(dev, "failed to determine PON rev: %d\n", ret);
+		return ret;
+	}
+
+	priv->revision = ret;
+	dev_dbg(dev, "PON rev: %d\n", priv->revision);
+
+	return 0;
+}
+
+static int pm8916_pon_bind(struct udevice *dev)
+{
+	int ret;
+
+	if (CONFIG_IS_ENABLED(BUTTON_QCOM_PMIC)) {
+		ret = button_qcom_pmic_setup(dev);
+		if (ret)
+			dev_warn(dev, "failed to bind qcom_pwrkey: %d\n", ret);
+	}
+
+	return 0;
+}
+
+static const struct udevice_id pm8916_pon_ids[] = {
+	{ .compatible = "qcom,pm8916-pon" },
+	{},
+};
+
+U_BOOT_DRIVER(pm8916_pon) = {
+	.name = "pm8916_pon",
+	.id = UCLASS_MISC,
+	.of_match = pm8916_pon_ids,
+	.bind = pm8916_pon_bind,
+	.probe = pm8916_pon_probe,
+	.priv_auto = sizeof(struct pm8916_pon_priv),
+};
