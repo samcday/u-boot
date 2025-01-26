b42a541a26e9cf2e32ab0d915e23fe571aa711ef
Sam Day <me@samcday.com>
2025-01-26 12:45:12 +0100

    drivers: misc: pm8916_pon: support setting reboot type
    This is to be used in conjunction with PSHOLD sysreset driver.
    
    This code manages the RST_CTL register block, which PM8916 uses to
    decide what action to take when PS_HOLD is de-asserted.

--- include/pm8916_pon.h
+++ include/pm8916_pon.h
@@ -1,7 +1,11 @@
 // SPDX-License-Identifier: GPL-2.0+
 
+#include <sysreset.h>
+
 struct pm8916_pon_priv {
     struct udevice *pmic;
     phys_addr_t base;
     u32 revision;
 };
+
+int pm8916_pon_set_reboot_type(enum sysreset_t reset_type);
