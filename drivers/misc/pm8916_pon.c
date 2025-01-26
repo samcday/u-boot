d605d9513ea117c10f5f1f773dfec55569fc1c69
Sam Day <me@samcday.com>
2025-01-26 12:45:12 +0100

    drivers: misc: pm8916_pon: support setting reboot type
    This is to be used in conjunction with PSHOLD sysreset driver.
    
    This code manages the RST_CTL register block, which PM8916 uses to
    decide what action to take when PS_HOLD is de-asserted.

--- drivers/misc/pm8916_pon.c
+++ drivers/misc/pm8916_pon.c
@@ -8,21 +8,101 @@
  * (C) Copyright 2025 Linaro Ltd.
  */
 
-#define LOG_DEBUG
-
 #include <dm.h>
 #include <dm/lists.h>
 #include <dm/device_compat.h>
 #include <power/pmic.h>
+#include <linux/delay.h>
+#include <sysreset.h>
 #include "button/qcom-pmic.h"
+#include "pm8916_pon.h"
 
 #define PON_REV2			0x01
+
+#define PON_PS_HOLD_RST_CTL		0x5a
+#define PON_PS_HOLD_RST_CTL2		0x5b
+
+#define PON_PS_HOLD_ENABLE		BIT(7)
+#define PON_PS_HOLD_TYPE_MASK		0x0f
+
+#define PON_PS_HOLD_TYPE_WARM_RESET	1
+#define PON_PS_HOLD_TYPE_SHUTDOWN	4
+#define PON_PS_HOLD_TYPE_HARD_RESET	7
 
 struct pm8916_pon_priv {
 	struct udevice *pmic;
 	phys_addr_t base;
 	u32 revision;
 };
+
+int pm8916_pon_set_reboot_type(enum sysreset_t reset_type)
+{
+	struct udevice *dev;
+	struct pm8916_pon_priv *priv;
+	int ret;
+	uint pmic_reset_type;
+	uint enable_reg;
+
+	ret = uclass_get_device_by_driver(UCLASS_MISC,
+					  DM_DRIVER_GET(pm8916_pon), &dev);
+	if (ret) {
+		dev_warn(dev, "couldn't find pm8916_pon device: %d\n", ret);
+		return ret;
+	}
+	if (!dev) {
+		dev_warn(dev, "couldn't find pm8916_pon device\n");
+		return -ENODEV;
+	}
+
+	priv = dev_get_priv(dev);
+
+	/* PMICs with revision 0 have the enable bit in same register as ctrl */
+	if (priv->revision == 0)
+		enable_reg = PON_PS_HOLD_RST_CTL;
+	else
+		enable_reg = PON_PS_HOLD_RST_CTL2;
+
+	switch (reset_type) {
+		case SYSRESET_POWER_OFF:
+			pmic_reset_type = PON_PS_HOLD_TYPE_SHUTDOWN;
+			break;
+		case SYSRESET_COLD:
+		case SYSRESET_POWER:
+			pmic_reset_type = PON_PS_HOLD_TYPE_HARD_RESET;
+			break;
+		default:
+			pmic_reset_type = PON_PS_HOLD_TYPE_WARM_RESET;
+	}
+
+	dev_dbg(dev, "Setting PON reboot type: %x\n", pmic_reset_type);
+
+	ret = pmic_clrsetbits(priv->pmic, priv->base + enable_reg,
+			      PON_PS_HOLD_ENABLE, 0);
+	if (ret) {
+		dev_warn(dev, "clear PON_PS_HOLD_ENABLE failed: %d\n", ret);
+		return ret;
+	}
+
+	/* Delay needed for disable to kick in. */
+	/* Kernel uses usleep_range(100, 1000), lk2nd waits 300usec */
+	udelay(300);
+
+	ret = pmic_clrsetbits(priv->pmic, priv->base + PON_PS_HOLD_RST_CTL,
+			      0, pmic_reset_type);
+	if (ret) {
+		dev_warn(dev, "RST_CTL set failed: %d\n", ret);
+		return ret;
+	}
+
+	ret = pmic_clrsetbits(priv->pmic, priv->base + enable_reg,
+			      0, PON_PS_HOLD_ENABLE);
+	if (ret) {
+		dev_warn(dev, "PON_PS_HOLD_ENABLE failed: %d\n", ret);
+		return ret;
+	}
+
+	return 0;
+}
 
 static int pm8916_pon_probe(struct udevice *dev)
 {
---
+++ include/pm8916_pon.h
@@ -0,0 +1,3 @@
+// SPDX-License-Identifier: GPL-2.0+
+
+int pm8916_pon_set_reboot_type(enum sysreset_t reset_type);
