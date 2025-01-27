1ee1d53d1b72e6e8378787b33b6ef7d613d33bca
Sam Day <me@samcday.com>
2025-01-27 14:05:14 +0100

    armv8: spin_table: add hook for booting cores
    Introduce the weak symbol spin_table_boot_cpu, which is called by
    spin_table_update_dt during pre-boot FDT fixup phase for each CPU that
    has enable-method="spin-table".
    
    This saves the board/arch code from needing to scan the DT looking for
    CPUs to boot, and also makes it easier to bring the secondary CPUs online
    as late as possible.
    
    Reviewed-by: Tom Rini <trini@konsulko.com>
    Signed-off-by: Sam Day <me@samcday.com>

--- arch/arm/include/asm/spin_table.h
+++ arch/arm/include/asm/spin_table.h
@@ -10,5 +10,6 @@ extern char spin_table_reserve_begin;
 extern char spin_table_reserve_end;
 
 int spin_table_update_dt(void *fdt);
+int spin_table_boot_cpu(void *fdt, int cpu_offset);
 
 #endif /* __ASM_SPIN_TABLE_H__ */
